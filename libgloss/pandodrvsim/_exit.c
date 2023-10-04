8ee25c866aa0860f77c16d2b23e8bb6c6d60ff32
Max Ruttenberg <mrutt@cs.washington.edu>
2023-10-04 12:57:42 -0700

    [pandodrvsim] exit system call implemented

--- libgloss/pandodrvsim/_exit.c
+++ libgloss/pandodrvsim/_exit.c
@@ -1,18 +1,10 @@
-/* Stub version of _exit.  */
+#include <machine/syscall.h>
+#include "internal_syscall.h"
 
-#include <limits.h>
-#include "config.h"
-#include <_ansi.h>
-#include <_syslist.h>
-
+/* Exit a program without cleaning up files.  */
 void
-_exit (int rc)
+_exit(int exit_status)
 {
-  /* Default stub just causes a divide by 0 exception.  */
-  int x = rc / INT_MAX;
-  x = 4 / x;
-
-  /* Convince GCC that this function never returns.  */
-  for (;;)
-    ;
+  syscall_errno (SYS_exit, 1, exit_status, 0, 0, 0, 0, 0);
+  while (1);
 }
